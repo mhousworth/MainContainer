e659597b760809039cb11a849e2263baaf92944c
arincon10912 <arincon10912@gmail.com>
2019-11-08 11:42:14 -0800

    The compiler warnings were fixed in this code + Phase one of compass code.

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+class Compass extends React.Component {
+
+  render() {
+    const direction = "Direction: \t" + this.props.direc;
+
+    return ( React.createElement('p', { className: "Compass" }, direction));
+  }
+}
+
 class GPS extends React.Component {
 
   render() {
     const latitude = "Latitude:\t" + this.props.latitude;
     const longitude = "Longitude\t" + this.props.longitude;
 
-    return(
+    return (
       React.createElement('div', null, [
-        React.createElement('p', null, latitude),
-        React.createElement('p', null, longitude)
+        React.createElement('p', {key:1}, latitude),
+        React.createElement('p', {key:2}, longitude)
       ])
     );
   }
+}
 
+class CurrentDraw extends React.Component {
+  render() {
+    const CurrentAmp = "Amps: " + this.props.amps;
+    return (React.createElement('p', { className: "CurrentDraw" }, CurrentAmp));
+  }
+}
 
+function UltrasonicSensor(props) {
+  return (
+    React.createElement('div', null, `Distance (feet): `, props.distance)
+  );
 }
 
 class MainContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude : 400,
-      longitude : 400
+      latitude: 400,
+      longitude: 400,
+      Amperage: 400,
+      distance: 400,
+      direction: 400,
     };
     setInterval(this.changeState, 1000);
   }
@@ -28,18 +51,32 @@ class MainContainer extends React.Component {
   changeState = () => {
     let lat = 1 - getRandomInt(3);
     let lon = 1 - getRandomInt(3);
+    let amp = 1 - getRandomInt(3);
+    let dis = 1 - getRandomInt(3);
+    let dir = 1 - getRandomInt(3);
     this.setState({
-      latitude : (this.state.latitude+lat),
-      longitude : (this.state.longitude+lon)
+      latitude: (this.state.latitude + lat),
+      longitude: (this.state.longitude + lon),
+      Amperage: (this.state.Amperage + amp),
+      distance: (this.state.distance + dis),
+      direction: (this.state.direction + dir),
     });
   }
 
   render() {
     const latitude = this.state.latitude;
     const longitude = this.state.longitude;
+    const amps = this.state.Amperage;
+    const dists = this.state.distance;
+    const direc = this.state.direction;
+    return (
+      React.createElement('div', null, [
+        React.createElement(GPS, { key:1, latitude, longitude }),
+        React.createElement(CurrentDraw, { key:2, amps }),
+        React.createElement(UltrasonicSensor, { key:3, distance: dists }),
+        React.createElement(Compass, { key:4, direc }),
+      ])
 
-    return(
-      React.createElement(GPS, {latitude, longitude} )
     );
   }
 
